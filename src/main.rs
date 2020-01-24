8b49d4c342c61ca83c4ca4ca39722bf79dcec967
Luc Bergeron <psyluche@gmail.com>
2020-01-24 15:32:47 -0500

    Got menu and add contact working.

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,74 @@ impl PhoneBook {
     // fn new(self) -> PhoneBook {
     //     self.phone_book = HashMap<String, PhoneEntry>::new()
     // }
-    fn add_contact(&self) {
+    fn add_contact(&mut self) {
         let mut name = String::new();
         let mut family_name = String::new();
         let mut phone_number = String::new();
 
-        print!("Please enter the name:");
+        println!("Please enter the name:");
         io::stdin().read_line(&mut name).unwrap();
+        println!();
 
-        print!("Please enter the family name:");
+        println!("Please enter the family name:");
         io::stdin().read_line(&mut family_name).unwrap();
+        println!();
         
-        print!("Please enter the phone number:");
+        println!("Please enter the phone number:");
         io::stdin().read_line(&mut phone_number).unwrap();        
+        println!();
 
-        self.phone_book.insert(name, PhoneEntry{name, family_name, phone_number});
+        let key = name.clone();
+        let entry = PhoneEntry {
+            name,
+            family_name,
+            phone_number,
+        };
+        self.phone_book.insert(key, entry);
     }
+}
+
+fn show_menu() -> i32 {
+    println!("Welcome to the phone book application version 0.1.");
+    println!("\t 1- Add contact.");
+    println!("\t 2- Remove a contact.");
+    println!("\t 3- Search contact by name.");
+    println!("\t 4- Search contact by phone number.");
+    println!("\t 5- Print phone book entries.");
+    println!("\t 6- Quit.");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)
+        .expect("Failed to read input from stdin...");
+
+    let menu_choice: i32 = match input.trim().parse() {
+        Ok(i) => return i,
+        Err(_) => {
+            println!("Invalid input. Please select a number between 1 and 6.");
+            return -1
+        }
+    };
 }
 
 
 fn main() {
-    let mut pb : PhoneBook;
+    let mut pb = PhoneBook {phone_book : HashMap::new()};
+    let mut menu_choice = 0;
 
-    pb.add_contact();
+    loop {
+        menu_choice = show_menu();
+        match menu_choice {
+            1 => pb.add_contact(),
+            2 => println!("Removing contact..."),
+            3 => println!("Searching contact..."),
+            4 => println!("Search V2 contact..."),
+            5 => println!("Print contact..."),
+            6 => {
+                println!("Good bye!");
+                break;
+            },
+            _ => {}
+        }
+    }
+
 }
